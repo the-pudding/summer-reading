339432274b903cdaf29c13250f293d13cc9ea9f4
russellgoldenberg <russellgoldenberg@gmail.com>
2019-06-12 20:52:04 -0400

    font and animation

--- src/js/head.js
+++ src/js/head.js
@@ -5,3 +5,14 @@ import './polyfills/endsWith';
 import './polyfills/findIndex';
 import './polyfills/find';
 import './polyfills/includes';
+
+import loadFont from './utils/load-font';
+
+const fonts = [
+  { family: 'Vast Shadow', weight: 400 },
+  { family: 'Righteous', weight: 400 },
+  { family: 'Unica One', weight: 400 },
+  { family: 'Abril Fatface', weight: 400 },
+];
+
+fonts.forEach(loadFont);
